c55b7b1a37189f0bfaf061fde85f12401f1f4be8
carlosninacio <cigramirez@poligran.edu.co>
2025-11-03 18:43:14 -0500

    Modelo para base de datos

---
+++ inventario-backend/src/main/java/cr/inventarios/modelo/Producto.java
@@ -0,0 +1,24 @@
+package cr.inventarios.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer idProducto;
+    String descripcion;
+    Double precio;
+    Integer existencia;
+}
