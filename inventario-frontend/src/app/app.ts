b9232ccb02453448dfb100b97af44d4ea2fbdfa5
carlosninacio <cigramirez@poligran.edu.co>
2025-11-16 17:03:02 -0500

    FIX

--- inventario-frontend/src/app/app.ts
+++ inventario-frontend/src/app/app.ts
@@ -3,9 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  templateUrl: './app.html'
 })
 export class App {
   protected readonly title = signal('inventario-frontend');
