d56462dde628fbfa694e8486d03ed3c19c7a73e5
carlosninacio <cigramirez@poligran.edu.co>
2025-11-16 21:07:57 -0500

    Creación servicio, repositorio y controlador en el back

---
+++ inventario-backend/src/main/java/cr/inventarios/repositorio/ProductoRepositorio.java
@@ -0,0 +1,8 @@
+package cr.inventarios.repositorio;
+
+import cr.inventarios.modelo.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoRepositorio extends JpaRepository<Producto, Integer> {
+
+}
